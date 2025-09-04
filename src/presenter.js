0f26fb3385eeda70f5e1e2901beb1edf353fb2e0
nicoleUg <sofia.ugarte@ucb.edu.bo>
2025-09-04 05:21:54 -0400

    feat(ui): formulario FizzBuzz con presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,11 @@
-import sumar from "./sumador";
+import { generateFizzBuzzString } from "./fizzbuzz.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#fizzbuzz-form");
+const input = document.querySelector("#numero");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const n = Number.parseInt(input.value, 10);
+  div.innerHTML = `<p>${generateFizzBuzzString(n)}</p>`;
 });
